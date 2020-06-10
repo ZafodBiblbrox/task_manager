f05d6d70bfd54c6ba7b2f3a5758a12440ddac33d
Artur Dent <integer.u@gmail.com>
2020-06-11 00:44:03 +0500

    Reworked user.py, implemeted tickets open and close methods, extra attrs were deleted

--- user.py
+++ user.py
@@ -1,18 +1,40 @@
+from task import Ticket
+import datetime
+
 class User:
 
-    def __init__(self, position, name, email, login, password, access_level):
-        self.position = position
-        self.name = name
-        self.email = email
-        self.login = login
-        self.password = password
-        self.access_level = access_level
+    def __init__(self, name=None):
+        self._login = None
+        self._password = None
+        self._name = name
+        self._assigned_tickets = []
 
-    def perform_login(self, login, password):
-        pass
+    @property
+    def name(self):
+        return self._name
 
-    def create_ticket(self):
-        pass
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    def log_in_system(self, login, password):
+        self._login = login
+        self._password = password
+
+    def create_ticket(self, task):
+        task_obj = Ticket(task)
+        task_obj.open_date = datetime.date.today()
+        self._assigned_tickets.append(task_obj)
+        task = task_obj.task
+        ticket_id = self._assigned_tickets.index(task)
+        task_obj.ticket_no = ticket_id
+
+    def define_closure_date(self, task_obj, year, month, day):
+        task_obj.closure_date = datetime.date(year, month, day)
+
+    def get_user_tickets(self):
+        user_tasks =  self._assigned_tickets
+        return user_tasks
 
     def receive_ticket(self, ticket):
         pass
@@ -23,9 +45,9 @@ class User:
     def extend_ticket_date(self, ticket):
         pass
 
-    def determine_closure_date(self, ticket):
-        pass
+
 
     def confirm_closure(self, ticket):
         pass
+
 
